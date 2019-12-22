69fe1bd88f0510899e8d7c6b7d7bbc82a4ed681e
charles loisea <charles.loiseau@epitech.eu>
2019-12-22 17:48:09 +0100

    changed api

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule('svg');
- 
-    svgRule.uses.clear();
- 
-    svgRule
-      .use('babel-loader')
-      .loader('babel-loader')
-      .end()
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader');
-  },
+  ]
 }
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,44 +1,64 @@
 <template>
   <div>
-    <h1>MIN {{ daily.Temperature.Maximum.Value }} °C</h1>
-    <h1>MAX {{ daily.Temperature.Minimum.Value }} °C</h1>
-    <h3></h3>
+    <div v-if="current !== undefined">
+      <h1>{{ current.main.temp }}°C</h1>
+      <h1>{{ current.weather[0].description }}</h1>
+      <h2>{{ current.main.feels_like }}°C</h2>
+      <h3>{{ current.main.temp_max + "/" + current.main.temp_min }}°C</h3>
+      <p>{{ current.main.humidity }}%</p>
+    </div>
+    <div v-if="forecast !== undefined">
+      <div 
+        v-for="item in forecast.list"
+        :key="item.key"
+      >
+        <h2>{{ getDate(item.dt) }}</h2>
+        <h3>{{ item.weather[0].description }}</h3>
+        <p>{{ item.main.temp_max + "/" + item.main.temp_min }}°C</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
-async function getDailyWeather() {
-    const response = await axios.get("http://dataservice.accuweather.com/forecasts/v1/daily/1day/133651?apikey=NZkRfJXuglJmtR6vYgdZFZ5bB6PfJAbV&metric=true")
-    // eslint-disable-next-line no-unused-vars
-    return response.data.DailyForecasts[0]
+async function getCurentWeather() {
+    const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?zip=93460,fr&units=metric&APPID=3acd0b10fd8f7ee76e05204efd0ce7af")
+    return response.data
 
 }
-async function getHourlyWeather() {
-    const response = await axios.get("http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/133651?apikey=NZkRfJXuglJmtR6vYgdZFZ5bB6PfJAbV&metric=true")
+async function getForecastWeather() {
+    const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast?zip=93460,fr&units=metric&APPID=3acd0b10fd8f7ee76e05204efd0ce7af")
     return response.data
 }
+
 export default {
     data() {
         return {
-            hourly: undefined,
-            daily: undefined
+            forecast: undefined,
+            current: undefined
         }
     },
     computed: {
     
     },
     mounted() {
-        getHourlyWeather().then((data) => {
-            this.hourly = data
+        getForecastWeather().then((data) => {
+            this.forecast = data
         })
-        getDailyWeather().then((data) => {
-            this.daily = data
+        getCurentWeather().then((data) => {
+            this.current = data
 
         })
-
     },
+    methods: {
+        getDate(date) {
+            moment.locale('fr')
+            return moment(date * 1000).calendar()
+        }
+    }
 
 
 }
